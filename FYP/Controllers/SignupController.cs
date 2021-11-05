40584e818937095a7c4e04bb5c8f1a1e9d6b4fa0
Karthik <k.artixc@gmail.com>
2021-11-05 21:35:46 +0800

    minor chages

---
+++ FYP/Controllers/SignupController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace FYP.Controllers
+{
+    public class SignupController : Controller
+    {
+        
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Thanks()
+        {
+            IFormCollection form = HttpContext.Request.Form;
+            string username = form["Username"].ToString().Trim();
+            string password = form["Password"].ToString().Trim();
+
+            ViewData["Username"] = username;
+            ViewData["Password"] = password;
+
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
