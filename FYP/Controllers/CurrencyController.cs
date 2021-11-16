5c90c64a35501aedc50a3b600d8d4a20396c3e0a
MAK JUN WAI, JASPER RP <19007578@myrp.edu.sg>
2021-11-16 23:36:41 +0800

    View all Currencies and Navbar (Jasper)

---
+++ FYP/Controllers/CurrencyController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using FYP.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Security.Claims;
+
+namespace FYP.Controllers
+{
+    public class CurrencyController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<CurrencyExchange> curList = DBUtl.GetList<CurrencyExchange>("SELECT * FROM ExchangeRates");
+            return View(curList);
+            
+        }
+    }
+}
---
+++ FYP/Models/CurrencyExchange.cs
@@ -0,0 +1,9 @@
+﻿namespace FYP.Models
+{
+    public class CurrencyExchange
+    {
+       public string Source_currency { get; set; }
+        public string Target_currency { get; set;}
+        public decimal Exchange_rate { get; set; }    
+    }
+}
