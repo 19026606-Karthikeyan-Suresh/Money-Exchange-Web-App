8182bed7a57910aa35906a0b5111418d884c9beb
Karthik <k.artixc@gmail.com>
2021-11-24 13:28:01 +0800

    Created transactionEdit and modified the csproj to use sql client

--- FYP/Controllers/TransactionController.cs
+++ FYP/Controllers/TransactionController.cs
@@ -41,7 +41,7 @@ namespace FYP.Controllers
             else
             {
                 string sql = @"INSERT INTO Transactions(Source_currency, Source_amount, Converted_currency, 
-Converted_amount, exchange_rate, Transaction_date) VALUES('{0}', {1}, '{2}', {3}, {4}, '{5:yyyy-MM-dd}')";
+Converted_amount, Exchange_rate, Transaction_date) VALUES('{0}', {1}, '{2}', {3}, {4}, '{5:yyyy-MM-dd}')";
 
                 string insert = String.Format(sql, TR.Source_currency.EscQuote(), TR.Source_amount,
                     TR.Converted_currency.EscQuote(), TR.Converted_amount, TR.Exchange_rate, TR.Transaction_date);
@@ -91,6 +91,61 @@ Converted_amount, exchange_rate, Transaction_date) VALUES('{0}', {1}, '{2}', {3}
                 }
             }
             return RedirectToAction("Index");
+        }
+        #endregion
+
+        #region "Transaction Edit" - Karthik
+        [Authorize]
+        public IActionResult TransactionEdit(int id)
+        {
+            string sql = @"SELECT * FROM Transactions WHERE Transaction_id={0}";
+
+            string select = String.Format(sql, id);
+            List<Transaction> TRlist = DBUtl.GetList<Transaction>(select);
+            if (TRlist.Count == 1)
+            {
+                Transaction TR = TRlist[0];
+                return View(TR);
+            }
+            else
+            {
+                TempData["Message"] = "Transaction Record does not exist";
+                TempData["MsgType"] = "warning";
+                return RedirectToAction("Index");
+            }
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult TransactionEdit(Transaction TR)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewData["Message"] = "Invalid Input";
+                ViewData["MsgType"] = "danger";
+                return View("TransactionEdit", TR);
+            }
+            else
+            {
+                string sql = @"UPDATE Transactions  
+                              SET Source_currency='{1}', Source_amount={2}, Converted_currency='{3}',
+                                  Converted_amount={4}, Exchange_rate={5}, Transaction_date='{6:yyyy-MM-dd}' 
+                            WHERE Transaction_id={0}";
+                string update = String.Format(sql, TR.Transaction_id, TR.Source_currency.EscQuote(), TR.Source_amount,
+                    TR.Converted_currency.EscQuote(), TR.Converted_amount, TR.Exchange_rate, TR.Transaction_date);
+
+                if (DBUtl.ExecSQL(update) == 1)
+                {
+                    TempData["Message"] = "Transaction Updated";
+                    TempData["MsgType"] = "success";
+                }
+                else
+                {
+                    TempData["Message"] = DBUtl.DB_Message;
+                    TempData["MsgType"] = "danger";
+                }
+                return RedirectToAction("Index");
+            }
         }
         #endregion
     }
