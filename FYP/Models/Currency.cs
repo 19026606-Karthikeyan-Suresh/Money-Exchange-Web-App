90ea32b21c6e1d14708811a75531ec1499271eaa
HUANG KAIWEN RP <19028252@myrp.edu.sg>
2021-11-24 13:23:51 +0800

    modify currency controller, model(currency, stock), view(currencyAdd, currencyDelete)

---
+++ FYP/Models/Currency.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FYP.Models
+{
+    public class Currency
+    {
+        public string  CurrencyName { get; set; }
+        public string Country { get; set; }
+        
+
+    }
+}
---
+++ FYP/Models/Stock.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FYP.Models
+{
+    public class Stock
+    {
+        public string StockID { get; set; }
+        public string  CurrenctName { get; set; }
+        public string CurrencyStock { get; set; }
+        public string AverageRate { get; set; }
+        
+
+    }
+}
