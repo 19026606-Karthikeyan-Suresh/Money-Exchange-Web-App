7e795b9d07b602aef045970cd351d1882ced2545
Teng Yik <19010398@myrp.edu.sg>
2021-11-27 21:25:35 +0800

    Model for account, views for addusers and index

---
+++ FYP/Models/Account.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FYP.Models
+{
+   public class Account
+   {
+        public string username { get; set; }
+        public string password { get; set; }
+        public string name { get; set; }
+        public string role { get; set; }
+        public DateTime dob { get; set; }
+        public int deleted { get; set; }
+        public string deleted_by { get; set; }
+    }
+}
+
+
