5a5d567ee9144f3f4d9724d15a2d7a5f00cad662
MAK JUN WAI, JASPER RP <19007578@myrp.edu.sg>
2021-12-04 14:03:51 +0800

    Jasper - Create an Enquiry

---
+++ MoneyExchangeWebApp/Controllers/EnquiriesController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using FYP.Models;
+
+namespace FYP.Controllers
+{
+    public class EnquiriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult FAQ()
+        {
+            List<FAQ> faqList = DBUtl.GetList<FAQ>("SELECT * FROM FAQ");
+            return View(faqList);
+        }
+
+        public IActionResult Enquire()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Enquire(Enquiry newEnquiry)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewData["Message"] = "Please fill up all the required information.";
+                ViewData["MsgType"] = "warning";
+            }
+            else
+            {
+                
+                string insert =
+                   @"INSERT INTO Enquiries(visitor_email_address, description, enquiry_date, status, answered_by, deleted, deleted_by) 
+                 VALUES('{0}', '{1}', '{2:dd/MM/yyyy}', {3}, '{4}', {5}, '{6}')";
+                string final = String.Format(@"INSERT INTO Enquiries(visitor_email_address, description, enquiry_date, status, answered_by, deleted, deleted_by) 
+                 VALUES('{0}', '{1}', '{2:dd/MM/yyyy}', {3}, '{4}', {5}, '{6}')", newEnquiry.visitor_email_address, newEnquiry.description, newEnquiry.enquiry_date
+                    , newEnquiry.status, newEnquiry.answered_by, newEnquiry.deleted, newEnquiry.deleted_by);
+                int result = DBUtl.ExecSQL(insert, newEnquiry.visitor_email_address, newEnquiry.description, newEnquiry.enquiry_date
+                    , newEnquiry.status, newEnquiry.answered_by, newEnquiry.deleted, newEnquiry.deleted_by);
+
+                if (result == 1)
+                {
+                    ViewData["Message"] = "Enquiry Submitted! We will reply to you soonest as possible! Thank You!";
+                    ViewData["MsgType"] = "success";
+                }
+                else
+                {
+                    ViewData["Message"] = DBUtl.DB_Message;
+                    ViewData["MsgType"] = "danger";
+                    ViewData["sql"] = final;
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ MoneyExchangeWebApp/Models/Enquiry.cs
@@ -0,0 +1,14 @@
+﻿namespace FYP.Models
+{
+    public class Enquiry
+    {
+        public int enquiry_id { get; set; }
+        public string visitor_email_address { get; set; }
+        public string description { get; set; }
+        public string enquiry_date { get; set; }
+        public byte status { get; set; }
+        public string answered_by { get; set; }
+        public byte deleted { get; set; }
+        public string deleted_by { get; set; }
+    }
+}
