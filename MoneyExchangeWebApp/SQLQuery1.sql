6571605229a81b06389344e37b88725f3cf2dae1
Karthik <k.artixc@gmail.com>
2022-01-24 18:07:59 +0800

    SQL query updated with quote currency

--- MoneyExchangeWebApp/SQLQuery1.sql
+++ MoneyExchangeWebApp/SQLQuery1.sql
@@ -6,7 +6,7 @@ DROP TABLE IF EXISTS Enquiries;
 DROP TABLE IF EXISTS Accounts;
 DROP TABLE IF EXISTS ExchangeRates;
 DROP TABLE IF EXISTS FAQ;
-DROP TABLE IF EXISTS Wallet;
+DROP TABLE IF EXISTS Stock;
 --
 -- Create tables
 --
@@ -30,11 +30,11 @@ CREATE TABLE Accounts(
 
 INSERT INTO Accounts(EmailAddress, Password, FirstName, LastName, Address, PhoneNumber, Gender, DOB, Role, DateCreated, Deleted, DeletedBy, DateDeleted) VALUES 
 ('john123@gmail.com', HASHBYTES('SHA1', 'password1'), 'John', 'gino', 'Blk 35 Mandalay Road # 13–37 Mandalay Towers Singapore 308215','87687908','Male','1999-01-12','staff', '2012-08-12',0, null, null),
-('kaiwen4399@gmail.com', HASHBYTES('SHA1', 'password2'), 'Kaiwen', 'Huang', 'Blk 35 Mandalay Road # 13–37 Mandalay Towers Singapore 308215','87687908','Male','2000-01-15', 'admin', '2013-05-02', 0, null, null),
-('k.artixc@gmail.com', HASHBYTES('SHA1', 'password3'), 'Karthikeyan', 'Suresh', 'Blk 35 Mandalay Road # 13–37 Mandalay Towers Singapore 308215','87687908','Male','2002-06-14','admin', '2015-05-04', 0, null, null),
-('Tengyik1763@gmail.com', HASHBYTES('SHA1', 'password3'), 'Yik', 'Teng', 'Blk 35 Mandalay Road # 13–37 Mandalay Towers Singapore 308215','87687908','Male','2002-01-19', 'admin', '2019-05-04', 0, null, null),
-('19007578@myrp.edu.sg', HASHBYTES('SHA1', 'password4'), 'Jasper', 'Mak Jun Wai', 'Blk 35 Mandalay Road # 13–37 Mandalay Towers Singapore 308215','87687908','Male','1999-01-12', 'admin', '2019-05-04' ,0, null, null),
-('DummyUser1@myrp.edu.sg', HASHBYTES('SHA1', 'password5'), 'Jaden', ' Wai', '21C Tanjong Pagar International', '87689013', 'Male','1999-01-12', 'user', '2022-01-03' ,0, null, null),
+('kaiwen4399@gmail.com', HASHBYTES('SHA1', 'password2'), 'Kaiwen', 'Huang', 'Blk 35 Mandalay Road # 13–37 Mandalay Towers Singapore 308215','87687908','Male','2000-01-15', 'admin','2013-05-02', 0, null, null),
+('k.artixc@gmail.com', HASHBYTES('SHA1', 'password3'), 'Karthikeyan', 'Suresh', 'Blk 35 Mandalay Road # 13–37 Mandalay Towers Singapore 308215','87687908','Male','2002-06-14','admin','2015-05-04', 0, null, null),
+('Tengyik1763@gmail.com', HASHBYTES('SHA1', 'password3'), 'Yik', 'Teng', 'Blk 35 Mandalay Road # 13–37 Mandalay Towers Singapore 308215','87687908','Male','2002-01-19', 'admin','2019-05-04',0 , null, null),
+('19007578@myrp.edu.sg', HASHBYTES('SHA1', 'password4'), 'Jasper', 'Mak Jun Wai', 'Blk 35 Mandalay Road # 13–37 Mandalay Towers Singapore 308215','87687908','Male','1999-01-12','admin', '2019-05-04' ,0 , null, null),
+('DummyUser1@myrp.edu.sg', HASHBYTES('SHA1', 'password5'), 'Jaden', ' Wai', '21C Tanjong Pagar International', '87689013', 'Male','1999-01-12', 'user','2022-01-03' ,0, null, null),
 ('DummyUser2@myrp.edu.sg', HASHBYTES('SHA1', 'password6'), 'Mark', ' Haedrig', '19D Tanjong Pagar International', '87680987', 'Male','2000-04-05', 'user', '2021-07-05' ,0, null, null),
 ('DummyUser3@myrp.edu.sg', HASHBYTES('SHA1', 'password7'), 'Amelia', ' Toh', 'Tanjong Pagar Bazar', '89761238', 'Female','2011-07-09', 'user', '2019-05-04' ,0, null, null),
 ('DummyUser4@myrp.edu.sg', HASHBYTES('SHA1', 'password8'), 'Amanda', ' Koh', 'Bedok South Park 1', '806184567', 'Unspecified','1999-07-08', 'user', '2018-02-04' ,0, null, null),
@@ -61,54 +61,55 @@ INSERT INTO Enquiries(EmailAddress, Subject, Question, EnquiryDate, Status, Answ
 
 
 CREATE TABLE FAQ(
-	FAQ_ID			INT	 			IDENTITY PRIMARY KEY,
+	FaqId			INT	 			IDENTITY PRIMARY KEY,
 	Question 		VARCHAR(200)	NOT NULL,
 	Answer			VARCHAR(200) 	NOT NULL,
-    Created_by		VARCHAR(32)  	NOT NULL,
+    CreatedBy		VARCHAR(32)  	NOT NULL,
 	Deleted			BIT				NOT NULL,
-    Deleted_by		VARCHAR(32)		NULL
+    DeletedBy		VARCHAR(32)		NULL
 	);
  
-    INSERT INTO FAQ(Question, Answer, Created_by, Deleted, Deleted_by) VALUES
+    INSERT INTO FAQ(Question, Answer, CreatedBy, Deleted, DeletedBy) VALUES
     ('What is the maximum amount that we can exchange per transaction?', 'There is no fixed price!, but do note if we do not have enough, we will take 3 working days to get back to you', 'John Wick', 0, null),
 	('How many currencies do you have?', 'We hold a variety of currencies with their respective exchange rates. Most are from ASEAN countries only. We apologise for any inconvenience', 'John Wick', 1, 'John Wick'),
 	('Do you have an exchange rate fee?', 'Yes! It is only 3% per transaction', 'John Wick', 0, null);
 
 CREATE TABLE ExchangeRates( 
-		Source_currency 	VARCHAR(5)      NOT NULL,
-    	Target_currency 	VARCHAR(5)      NOT NULL,
-    	Exchange_rate   	Float 	NOT NULL
+		BaseCurrency 	VARCHAR(5)      NOT NULL,
+		QuoteCurrency 	VARCHAR(5)      NOT NULL,
+    	ExchangeRate   	Float 			NOT NULL
 );
 
 CREATE TABLE Transactions(
-	Transaction_id 	      INT 	    	IDENTITY PRIMARY KEY,
-	Source_currency	      VARCHAR(5)    NOT NULL,
-	Source_amount 	      DECIMAL(9,2)  NOT NULL,
-	Converted_currency    VARCHAR(5)	NOT NULL,
-	Converted_amount      DECIMAL(9,2)  NOT NULL,
-	Exchange_rate		  DECIMAL(9,2)  NOT NULL,
-	Transaction_date      DATE 			NOT NULL,
-    Created_by			  VARCHAR(32)	NOT NULL,
+	TransactionId 	      INT 	    	IDENTITY PRIMARY KEY,
+	BaseCurrency	      VARCHAR(5)    NOT NULL,
+	BaseAmount	 	      DECIMAL(9,2)  NOT NULL,
+	QuoteCurrency		  VARCHAR(5)	NOT NULL,
+	QuoteAmount			  DECIMAL(9,2)  NOT NULL,
+	ExchangeRate		  DECIMAL(9,2)  NOT NULL,
+	TransactionDate		  DATE 			NOT NULL,
+	AccountId			  INT			NOT NULL,
+    DoneBy  			  VARCHAR(32)	NOT NULL,
 	Deleted				  BIT			NOT NULL,
-    Deleted_by			  VARCHAR(32) 	NULL
+    DeletedBy			  VARCHAR(32) 	NULL
 );
 
-INSERT INTO Transactions(Source_currency, Source_amount, Converted_currency, Converted_amount, Exchange_rate, Transaction_date, Created_by, Deleted, Deleted_by) VALUES
-('SGD', 10.00, 'MYR', 30.77, 3.08, '2021-11-16', 'John Wick', 0, null),
-('SGD', 1200000.00, 'CNY', 5662411.20, 4.72, '2021-11-15', 'James Charles' ,0, null), 
-('SGD', 123.00, 'MMK', 160774.14, 1307.11, '2021-11-14', 'Charlene Lim', 0, null),
-('SGD', 120.00, 'MMK', 156853.20, 1307.11, '2021-01-14', 'Charlene Lim', 1, 'Kanye West');
+INSERT INTO Transactions(BaseCurrency, BaseAmount, QuoteCurrency, QuoteAmount, ExchangeRate, TransactionDate, AccountId, DoneBy, Deleted, DeletedBy) VALUES
+('SGD', 10.00, 'MYR', 30.77, 3.08, '2021-11-16', 1,'John Gino', 0, null),
+('SGD', 1200000.00, 'CNY', 5662411.20, 4.72, '2021-11-15', 3,'Karthikeyan Suresh' ,0, null), 
+('SGD', 123.00, 'MMK', 160774.14, 1307.11, '2021-11-14', 3,'Karthikeyan Suresh', 0, null),
+('SGD', 120.00, 'MMK', 156853.20, 1307.11, '2021-01-14', 3,'Karthikeyan Suresh', 1, 'Ng Yik Teng');
 
-CREATE TABLE Wallet(
-	WalletId	INT         	IDENTITY PRIMARY KEY,
-	Email		VARCHAR(200)	NOT NULL,
+CREATE TABLE Stock(
+	StockId		INT         	IDENTITY PRIMARY KEY,
+	AccountId	INT				NOT NULL,
 	ISO			VARCHAR(3)		NOT NULL,
 	Amount		Float			NOT NULL,
 
 );
-INSERT INTO Wallet(Email, ISO, Amount) VALUES
-('k.artixc@gmail.com','SGD', 100000.00),
-('k.artixc@gmail.com','MMK', 112345.00),
-('k.artixc@gmail.com','CNY', 12345.00),
-('k.artixc@gmail.com','MYR', 98760.00),
-('Tengyik1763@gmail.com','SGD', 1000000.00);
+INSERT INTO Stock(AccountId, ISO, Amount) VALUES
+(1,'SGD', 100000.00),
+(1,'MMK', 112345.00),
+(1,'CNY', 12345.00),
+(1,'MYR', 98760.00),
+(2,'SGD', 1000000.00);
