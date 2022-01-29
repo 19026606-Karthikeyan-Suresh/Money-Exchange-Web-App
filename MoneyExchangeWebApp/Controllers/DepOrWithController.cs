7fff7603d2241f58ac948d89c9f087b5c7bd297c
Karthik <k.artixc@gmail.com>
2022-01-29 14:32:19 +0800

    Updated all of the controller, models and views to SQL query parameters. Updated the sql with different positions for each parameter

--- MoneyExchangeWebApp/Controllers/DepOrWithController.cs
+++ MoneyExchangeWebApp/Controllers/DepOrWithController.cs
@@ -13,102 +13,17 @@ namespace MoneyExchangeWebApp.Controllers
 {
     public class DepOrWithController : Controller
     {
-        /*#region "View List of stock" - Karthik
-        [Authorize(Roles ="admin, user")]
-        public IActionResult StocksOwnedByOwner()
+        #region List all transactions for specific currency - Karthik
+        public IActionResult GetAllDOWTransactions(int id)
         {
-            string email = User.Identity.Name;
-            List<Account> Alist = DBUtl.GetList<Account>(String.Format(@"SELECT * FROM Accounts WHERE EmailAddress='{0}'", email.EscQuote()));
-
-            if (Alist.Count == 1)
-            {
-                var account = Alist[0];
-                int AccId = account.AccountId;
-                List<Stock> curList = DBUtl.GetList<Stock>(String.Format("SELECT * FROM Stock WHERE AccountId={0}", AccId));
-                if (curList.Count == 1)
-                {
-                    return View(curList);
-                }
-                else
-                {
-                    return NotFound();
-                }
-            }
-            else
-            {
-                return NotFound();
-            }
-
-        }
-        #endregion*/
-
-/*        public IActionResult StockAdd(int id)
-        {
-            string sql = @"SELECT * FROM Stock WHERE Stock_id={0}";
-
-            string select = String.Format(sql, id);
-            List<Stock> Stocklist = DBUtl.GetList<Stock>(select);
-            if (Stocklist.Count == 1)
-            {
-                Stock S = Stocklist[0];
-                return View(S);
-            }
-            else
-            {
-                TempData["Message"] = "Stock does not exist";
-                TempData["MsgType"] = "warning";
-                return RedirectToAction("StockIndex");
-            }
-        }*/
-
-        #region "Edit Stock" - Karthik
-        [Authorize]
-        public IActionResult StockEdit(int id)
-        {
-            string sql = @"SELECT * FROM Stock WHERE Stock_id={0}";
-
-            string select = String.Format(sql, id);
-            List<Stock> Stocklist = DBUtl.GetList<Stock>(select);
-            if (Stocklist.Count == 1)
-            {
-                Stock S = Stocklist[0];
-                return View(S);
-            }
-            else
-            {
-                TempData["Message"] = "Stock does not exist";
-                TempData["MsgType"] = "warning";
-                return RedirectToAction("StockIndex");
-            }
+            string sql = @"SELECT * FROM DepWithTransactions WHERE StockId={0}";
+            List<DepWithTransactions> DWTlist = DBUtl.GetList<DepWithTransactions>(sql, id);
+            return Json(new {data = DWTlist});
         }
 
-        [Authorize]
-        [HttpPost]
-        public IActionResult StockEdit(Stock S)
+        public IActionResult DOWTransactions()
         {
-            if (!ModelState.IsValid)
-            {
-                ViewData["Message"] = "Invalid Input";
-                ViewData["MsgType"] = "danger";
-                return View("StockEdit", S);
-            }
-            else
-            {
-                string sql1 = @"UPDATE Stock SET Amount={1} WHERE StockId={0}";
-                string update = String.Format(sql1,S.StockId, S.Amount);
-                string sql2 = @"INSERT INTO DepWithTransactions ";
-                if (DBUtl.ExecSQL(update) == 1)
-                {
-                    TempData["Message"] = "Stock Updated";
-                    TempData["MsgType"] = "success";
-                }
-                else
-                {
-                    TempData["Message"] = DBUtl.DB_Message;
-                    TempData["MsgType"] = "danger";
-                }
-                return RedirectToAction("StockIndex");
-            }
+            return View();
         }
         #endregion
     }
--- MoneyExchangeWebApp/Models/DepWithTransactions.cs
+++ MoneyExchangeWebApp/Models/DepWithTransactions.cs
@@ -5,17 +5,16 @@ namespace MoneyExchangeWebApp.Models
     public class DepWithTransactions
     {
         public int TransactionId { get; set; }
-        public string EmailAddress { get; set; }
+        public int StockId { get; set; }
+        public string ISO { get; set; }
         public int DepOrWith { get; set; }
-        public string Currency { get; set; }
-        public float DepositAmt { get; set; }
-        public float WithdrawalAmt { get; set; }
+        public double Amount { get; set; }
+        public string DoneBy { get; set; }
         public DateTime TransactionDate { get; set; }
         public string EditedBy { get; set; }
         public DateTime EditedDate { get; set; }
         public int Deleted { get; set; }
         public string DeletedBy { get; set; }
         public DateTime DeletedDate { get; set; }
-
     }
 }
