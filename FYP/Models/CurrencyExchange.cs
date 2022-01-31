bed1d6475c761b87a762bdaad52efc014909c13c
Karthik <k.artixc@gmail.com>
2022-01-31 21:10:45 +0800

    Deleted FYP folder

--- FYP/Models/CurrencyExchange.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FYP.Models
-{
-    public class CurrencyExchange
-    {
-       public string Source_currency { get; set; }
-        public string Target_currency { get; set;}
-        public decimal Exchange_rate { get; set; }    
-    }
-}
