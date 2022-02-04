d72ab26ff246c0a15bb74ed6d846b1ff5a31c94f
Karthik <k.artixc@gmail.com>
2022-02-04 19:41:49 +0800

    Added necessary controllers, modesl and views to the project

---
+++ KachingExpress/wwwroot/js/DatatablesJS/Enquiry.js
@@ -0,0 +1,30 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#EnqTbl').DataTable({
+        "ajax": {
+            "url": "/Enquiry/GetAllEnquiries"
+        },
+        "columns": [
+            { "data": "enquiryId", "width": "5%" },
+            { "data": "emailAddress", "width": "10%" },
+            { "data": "subject", "width": "5%" },
+            { "data": "question", "width": "10%" },
+            { "data": "enquiryDate", "width": "5%" },
+            { "data": "status", "width": "5%" },
+            {
+                "data": "enquiryId",
+                "render": function (data) {
+                    return `
+                        <a href="/Enquiry/EnquiryReply/${data}"><i class="fas fa-reply"></i></a>
+                            `
+                },
+                "width": "10%"
+            }
+        ]
+    });
+}
