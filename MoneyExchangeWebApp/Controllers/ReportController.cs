c39ebcb1f4834ee5a7935fa65b74c4c48d718acf
Teng Yik <19010398@myrp.edu.sg>
2022-02-05 21:20:58 +0800

    modified Account and Report Controllers modified Account model and added BOS model modified Report folder modified ForgotPassword View

--- MoneyExchangeWebApp/Controllers/ReportController.cs
+++ MoneyExchangeWebApp/Controllers/ReportController.cs
@@ -7,79 +7,50 @@ namespace MoneyExchangeWebApp.Controllers
 {
     public class ReportController : Controller
     {
+
+        //Top or Worst 5 Currencies
         public IActionResult TradesDashboard()
         {
             return View();
         }
 
-        public IActionResult RedirectTrades()
+        public IActionResult DisplayCurrencies()   
         {
+
             IFormCollection form = HttpContext.Request.Form;
 
             string bestorworst = form["BestOrWorst"].ToString().Trim();
+            string month = form["Month"].ToString().Trim();
+            int apple = Int32.Parse(month);
+
+            string sql = "";
+            string select = "";
 
             if (bestorworst.Equals("Best"))
             {
-                return RedirectToAction("Top5Currencies");
+                sql = @"SELECT TOP(5) BaseCurrency AS 'ISO', COUNT(*) AS 'No. of Trades' FROM ConvTransactions 
+                           WHERE MONTH(TransactionDate) = {0}
+                           GROUP BY BaseCurrency ORDER BY 'No. of Trades' DESC ";
+
+                TempData["BestorWorst"] = "best";
+                select = String.Format(sql, apple);
+
             }
 
             if (bestorworst.Equals("Worst"))
             {
-                return RedirectToAction("Worst5Currencies");
-            }
-
-            return View();
-        }
-
-        public IActionResult Top5Currencies()
-        {
-            return View();
-        }
-        public IActionResult Worst5Currencies()
-        {
-            return View();
-        }
-        public IActionResult DisplayTopCurrencies()   //need to use TOP(5) as well
-        {
-
-            IFormCollection form = HttpContext.Request.Form;
-
-            string month = form["Month"].ToString().Trim();
-
-            int apple = Int32.Parse(month);
-
-            string sql = @"SELECT TOP(5) BaseCurrency AS 'ISO', COUNT(*) AS 'No. of Trades' FROM ConvTransactions 
-                           WHERE MONTH(TransactionDate) = {0}
-                           GROUP BY BaseCurrency ORDER BY 'No. of Trades' DESC ";
-
-            string select = String.Format(sql, apple);
-
-
-            DataTable dt = DBUtl.GetTable(select);
-
-            return View(dt);
-        }
-
-        public IActionResult DisplayWorstCurrencies()  //need to use TOP(5) as well
-        {
-            IFormCollection form = HttpContext.Request.Form;
-
-            string month = form["Month"].ToString().Trim();
-
-            int apple = Int32.Parse(month);
-
-            string sql = @"SELECT TOP(5) BaseCurrency AS 'ISO', COUNT(*) AS 'No. of Trades' FROM ConvTransactions 
+                sql = @"SELECT TOP(5) BaseCurrency AS 'ISO', COUNT(*) AS 'No. of Trades' FROM ConvTransactions 
                            WHERE MONTH(TransactionDate) = {0}
                            GROUP BY BaseCurrency ORDER BY 'No. of Trades' ASC ";
 
-            string select = String.Format(sql, apple);
-
+                TempData["BestorWorst"] = "worst";
+                select = String.Format(sql, apple);
+            }
 
             DataTable dt = DBUtl.GetTable(select);
 
             return View(dt);
         }
-
 
         //Top or Worst 5 Trading Days by Month
 
@@ -88,73 +59,40 @@ namespace MoneyExchangeWebApp.Controllers
             return View();
         }
 
-        public IActionResult RedirectDays()
+        public IActionResult DisplayDays()
         {
+            
             IFormCollection form = HttpContext.Request.Form;
 
             string bestorworst = form["BestOrWorst"].ToString().Trim();
+            string month = form["Month"].ToString().Trim();
+            int apple = Int32.Parse(month);
+
+            string sql = "";
+            string select = "";
 
             if (bestorworst.Equals("Best"))
             {
-                return RedirectToAction("TopDays");
-            }
-
-            if (bestorworst.Equals("Worst"))
-            {
-                return RedirectToAction("WorstDays");
-            }
-
-            return View();
-        }
-
-        public IActionResult TopDays()
-        {
-            return View();
-        }
-
-        public IActionResult WorstDays()
-        {
-            return View();
-        }
-
-        public IActionResult DisplayTopDays()
-        {
-            IFormCollection form = HttpContext.Request.Form;
-
-            string month = form["Month"].ToString().Trim();
-
-            int apple = Int32.Parse(month);
-
-            string sql = @"SELECT TOP(5) TransactionDate AS 'Day', COUNT(TransactionId) AS 'No. of Trades'
+                sql = @"SELECT TOP(5) TransactionDate AS 'Day', COUNT(TransactionId) AS 'No. of Trades'
                            FROM ConvTransactions
                           WHERE MONTH(TransactionDate) = {0}
                           GROUP BY TransactionDate ORDER BY 'No. of Trades' DESC";
 
+                TempData["BestorWorst"] = "best";
+                select = String.Format(sql, apple);
 
-            string select = String.Format(sql, apple);
+            }
 
-
-            DataTable dt = DBUtl.GetTable(select);
-
-            return View(dt);
-        }
-
-        public IActionResult DisplayWorstDays()
-        {
-            IFormCollection form = HttpContext.Request.Form;
-
-            string month = form["Month"].ToString().Trim();
-
-            int apple = Int32.Parse(month);
-
-            string sql = @"SELECT TOP(5) TransactionDate AS 'Day', COUNT(TransactionId) AS 'No. of Trades'
+            if (bestorworst.Equals("Worst"))
+            {
+                sql = @"SELECT TOP(5) TransactionDate AS 'Day', COUNT(TransactionId) AS 'No. of Trades'
                            FROM ConvTransactions
                           WHERE MONTH(TransactionDate) = {0}
                           GROUP BY TransactionDate ORDER BY 'No. of Trades' ASC";
 
-
-            string select = String.Format(sql, apple);
-
+                TempData["BestorWorst"] = "worst";
+                select = String.Format(sql, apple);
+            }
 
             DataTable dt = DBUtl.GetTable(select);
 
