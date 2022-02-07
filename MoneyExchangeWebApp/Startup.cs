44583ccf3b3c76d4d4472655465a7bff4c786aa3
Karthik <k.artixc@gmail.com>
2022-02-08 00:19:48 +0800

    Revert "Updated ConvTransactionIndex and CreateConvTransaction"
    This reverts commit 6705dc107c6ecb97c4a59f20bc85780878f3d014.

--- MoneyExchangeWebApp/Startup.cs
+++ MoneyExchangeWebApp/Startup.cs
@@ -1,53 +1,37 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Configuration;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.Authentication.Cookies;
-
-
 
 namespace MoneyExchangeWebApp
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-
-
-        // This method gets called by the runtime. Use this method
-        // to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
-            services.AddAuthentication(
-            CookieAuthenticationDefaults.AuthenticationScheme)
-            .AddCookie(options =>
-            {
-                options.LoginPath = "/Account/Login/";
-                options.AccessDeniedPath = "/Account/Forbidden/";
-            });
-
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                    .AddCookie(options => {
+                        options.LoginPath = "/Account/Login/";
+                        options.AccessDeniedPath = "/Account/Forbidden/";
+                    });
         }
 
-        // This method gets called by the runtime. Use this method to
-        // configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDefaultFiles(); // for index.html in wwwroot
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
             app.UseStaticFiles();
             app.UseAuthentication();
-            app.UseRouting();
-            app.UseAuthorization();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
