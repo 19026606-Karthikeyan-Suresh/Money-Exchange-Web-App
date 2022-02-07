2e39aefa17b7b690f1c30ef0a1eaba5b1721ff8a
Karthik <k.artixc@gmail.com>
2022-02-08 04:27:46 +0800

    Revert "Revert "Created new folder named KachingExpress. The new home page is displayed in this folder. Sending it in bulk. Updated sql for both as well.""
    This reverts commit 8db0d2483f7fb3661814a88d80d5122463932cda.

---
+++ KachingExpress/Models/UserLogin.cs
@@ -0,0 +1,15 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace KachingExpress.Models
+{
+   public class UserLogin
+   {
+      [Required(ErrorMessage = "Email Address cannot be empty!")]
+      public string UserID { get; set; }
+
+      [Required(ErrorMessage = "Password cannot be empty!")]
+      public string Password { get; set; }
+   }
+}
+
---
+++ KachingExpress/Utils/WebUtl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Dynamic;
+using System.Net;
+using Newtonsoft.Json;
+
+public static class WebUtl
+{
+    public static dynamic CallWebApi(string url)
+    {
+        WebClient client = new WebClient();
+        client.Headers["Accept"] = "application/json";
+
+        try
+        {
+            string datastr = client.DownloadString(new Uri(url));
+            //return ObjectConverter.Convert(datastr);
+
+            return JsonConvert.DeserializeObject(datastr);
+        }
+        catch (Exception ex)
+        {
+            return null;
+        }
+    }
+}
