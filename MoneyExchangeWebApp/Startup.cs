6705dc107c6ecb97c4a59f20bc85780878f3d014
Karthik <k.artixc@gmail.com>
2022-02-07 16:31:31 +0800

    Updated ConvTransactionIndex and CreateConvTransaction

--- MoneyExchangeWebApp/Startup.cs
+++ MoneyExchangeWebApp/Startup.cs
@@ -1,37 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.Authentication.Cookies;
+
+
 
 namespace MoneyExchangeWebApp
 {
     public class Startup
     {
-        public void ConfigureServices(IServiceCollection services)
+        public Startup(IConfiguration configuration)
         {
-            services.AddMvc(options => options.EnableEndpointRouting = false);
-            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-                    .AddCookie(options => {
-                        options.LoginPath = "/Account/Login/";
-                        options.AccessDeniedPath = "/Account/Forbidden/";
-                    });
+            Configuration = configuration;
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public IConfiguration Configuration { get; }
+
+
+
+        // This method gets called by the runtime. Use this method
+        // to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
         {
-            if (env.IsDevelopment())
+            services.AddControllersWithViews();
+            services.AddAuthentication(
+            CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie(options =>
             {
-                app.UseDeveloperExceptionPage();
-            }
+                options.LoginPath = "/Account/Login/";
+                options.AccessDeniedPath = "/Account/Forbidden/";
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to
+        // configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDefaultFiles(); // for index.html in wwwroot
             app.UseStaticFiles();
             app.UseAuthentication();
-            app.UseMvcWithDefaultRoute();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
